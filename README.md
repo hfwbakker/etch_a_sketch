6e3390f1c2527ff70521620baf3085b63fa60946
Henri Bakker <hfwbakker@gmail.com>
2020-12-02 22:59:47 +0800

    added grid generation logic + button

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ etch a sketch project for odin
 Odin requirements:
 https://www.theodinproject.com/lessons/etch-a-sketch-project
 
+CURRENT:
+created logic that allows for on demand grid generation, including a button to do just that. Important next todos: 
 
+1) make sure that if someone selects amount of rows and amount of columns it actually creates the right amount of rows and columns rather than just the right amount of divs
+2) somehow make sure that however many rows and columns are chosen they take up 100% of the width and height
+
+
+OLD:
 made it to point 3: add a hover effect. did it succesfully in CSS but maybe goot to try and do it with JS as well. you know, for science.
--- script.js
+++ script.js
@@ -10,10 +10,31 @@ const container = document.createElement("div");
 container.classList.add("mamaDiv");
 hotBody.appendChild(container);
 
+gridSize = 256
 
-for (let i = 1; i <= 256; i++){
-    let testing = document.createElement("div");
-    testing.classList.add("lilDiv")
-    testing.textContent = `Div ${i}`;
-    container.appendChild(testing);
+const gridGenerator = function(gridSize) { 
+    console.log("executing gridGenerator()")
+    container.innerHTML = ''
+    for (let i = 1; i <= gridSize; i++){
+        let testing = document.createElement("div");
+        testing.classList.add("lilDiv")
+        testing.textContent = `Div ${i}`;
+        container.appendChild(testing);
+    }
 }
+
+const chooseGridSize = function(a) {
+    console.log("executing chooseGridSize()")
+    a = prompt("how many rows and columns?");
+    let gridSize = (a * a);
+    console.log(gridSize);
+    return gridGenerator(gridSize);
+}
+
+const click = document.querySelector("#dasButton");
+click.addEventListener("click", () => {
+    console.log("CLICK");
+    chooseGridSize();
+});
+
+gridGenerator(gridSize)
